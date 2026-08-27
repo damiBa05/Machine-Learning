032229683543c5db817e694f51e40cbbf6da6f1e
damiBa05 <damianbarrantes05@gmail.com>
2026-08-27 14:04:50 -0600

    Second iteration header of DQN ready, start of the DQN. cpp file

--- TicTacToe/DQN.hpp
+++ TicTacToe/DQN.hpp
@@ -1,3 +1,21 @@
 #pragma once
-
 #include <vector>
+
+class DQN
+{
+private:
+    std::vector<std::vector<double>> weights;
+    std::vector<double> bias;
+    int numActions;
+    int numCells;
+public:
+    DQN(int actions, int cells);
+    double getQValue(int action, const std::vector<int>& cells) const;
+    void setWeight(int action, int cellIndex, double value);
+    void setBias(int action, double value);
+    void update(int action, const std::vector<int>& cells, double reward,
+        const std::vector<int>& nextCells, double learningRate, double discountFactor);
+    int selectAction(const std::vector<int>& cells, double epsilon,
+         const std::vector<int>& validActions) const;
+    void reset();
+};
